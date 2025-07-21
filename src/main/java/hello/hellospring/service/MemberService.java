789b74aca98b14d177ecfd2520dd4083a179e157
perfectoryinc <perfectoryinc@gmail.com>
2025-07-21 13:19:52 +0900

    feat: MemberServiceTest 테스트 코드 작성
    - DI 방식으로 변경
    - BeforeEach로 의존성 주입
    - AfterEach로 .clearStore();

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -8,7 +8,11 @@ import java.util.Optional;
 
 public class MemberService {
 
-    private final MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemoryMemberRepository memberRepository;
+
+    public MemberService(MemoryMemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     /**
      * 회원 가입
@@ -35,7 +39,7 @@ public class MemberService {
         return memberRepository.findAll();
     }
 
-    public Optional<Member> findMemberById(Long memberId) {
+    public Optional<Member> findOne(Long memberId) {
         return memberRepository.findById(memberId);
     }
 }
