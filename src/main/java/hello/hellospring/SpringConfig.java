c4468c6e218b92553d891c521168c364066f47fd
perfectoryinc <perfectoryinc@gmail.com>
2025-07-22 13:36:45 +0900

    feat: JDBC 템플릿 활용
    - JDBC 템플릿을 통해 테스트 코드 작성

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -28,6 +28,7 @@ public class SpringConfig {
     @Bean
     public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
+//        return new JdbcMemberRepository(dataSource);
         return new JdbcMemberRepository(dataSource);
     }
 }
